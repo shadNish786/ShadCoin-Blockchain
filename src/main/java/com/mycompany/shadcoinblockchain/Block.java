3f134a4add2f3c441140c9177cc6682550665af8
NishadMoodley786 <ST10410007@vcconnect.edu.za>
2025-02-15 00:52:11 +0200

    added code

--- src/main/java/com/mycompany/shadcoinblockchain/Block.java
+++ src/main/java/com/mycompany/shadcoinblockchain/Block.java
@@ -17,6 +17,7 @@ public class Block {
     public String previousHash; // holds previous blocks hash
     private String data; //data is set to a simple message and hold block data
     private long timeStamp; // as number of milliseconds since 1/1/1970
+    private int slave;
     
     
     //constructor for block
@@ -24,6 +25,25 @@ public class Block {
         this.data =data;
         this.previousHash = previousHash;
         this.timeStamp = new Date().getTime();
+        this.hash = calculateHash();//Making sure we do this after we set the other values.
         
     }
+    
+    
+    //new class to calculate hash 
+    public String calculateHash(){
+        String calculateHash = StringUtil.applySha256(previousHash+ Long.toString(timeStamp)+data);
+        return calculateHash;
+    }
+    
+    //mining code 
+    public void mineBlock(int difficulty){
+        String target = new String(new char[difficulty]).replace('\0','0'); //Create a string with difficulty * "0" 
+        while(!hash.substring(0,difficulty).equals(target)) {
+                slave ++;
+                hash = calculateHash();
+        }
+        System.out.println("Block Mined!!! : " + hash);
+    }
+  
 }
--- src/main/java/com/mycompany/shadcoinblockchain/StringUtil.java
+++ src/main/java/com/mycompany/shadcoinblockchain/StringUtil.java
@@ -9,15 +9,15 @@ package com.mycompany.shadcoinblockchain;
 import java.security.MessageDigest;
 
 
-
-public class StringUtil {
+// class to get text and apply sha256 for block data
+public class StringUtil{
       //applies shad256 to a string and returns results 
     public static String applySha256(String input){
         try {
             MessageDigest digest = MessageDigest.getInstance("SHA-256");
             //Applies sha256 to out input 
             byte[] hash = digest.digest(input.getBytes("UTF-8"));
-            StringBuffer hexString = new StringBuffer();
+            StringBuffer hexString = new StringBuffer(); //this will contain hash as hexidecimal value
             for (int i = 0;i < hash.length;i++){
                 String hex = Integer.toHexString(0xff & hash[i]);
                 if(hex.length() ==1){
